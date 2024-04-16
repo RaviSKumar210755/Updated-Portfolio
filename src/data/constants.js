18b88bfcde655ab82c6721cfe62c1273f317453e
RaviSKumar210755 <133785196+RaviSKumar210755@users.noreply.github.com>
2024-04-16 09:59:02 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/RaviSKumar210755",
   resume:
-    "https://drive.google.com/file/d/10VrU34v8xkh9sy8gbLvA60N9O0thJOFC/view",
+    "https://drive.google.com/file/d/1R8SQJbmwbvi18xjBMbZ2CUPfaJuWR86v/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ravishankar-kumar-664778236/",
   twitter: " ",
   insta: " ",
