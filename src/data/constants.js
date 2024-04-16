8818acd460e9f01f5f2ea3a04d155672ef3f0054
unknown <ravisiitism@gmail.com>
2024-04-16 08:43:47 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,343 @@
+export const Bio = {
+  name: "Ravishankar Kumar",
+  roles: ["Full Stack Developer", "competitive Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/RaviSKumar210755",
+  resume:
+    "https://drive.google.com/file/d/10VrU34v8xkh9sy8gbLvA60N9O0thJOFC/view",
+  linkedin: "https://www.linkedin.com/in/ravishankar-kumar-664778236/",
+  twitter: " ",
+  insta: " ",
+  facebook: " ",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image: "https://i.postimg.cc/FHYrgdc8/materialui.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://i.postimg.cc/mk6xtjdD/express.png",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "C",
+        image: "https://i.postimg.cc/pd11QrfG/c.png",
+      },
+      {
+        name: "C++",
+        image: "https://i.postimg.cc/bYGf5cdS/c.png",
+      },
+      {
+        name: "Advanced Data Structure and Algorithm",
+        image: "https://i.postimg.cc/02dWWcGJ/dsa.png",
+      },
+
+      {
+        name: "Git",
+        image: "https://i.postimg.cc/yYJtLFQm/git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://i.postimg.cc/MGwLZgW5/postman.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://i.ibb.co/0BfGDGC/images-1-nvcti.jpg",
+    role: "Full Stack Web Developer",
+    company: "NVCTI IIT Dhanbad",
+    date: "June 2022 - Present",
+    desc: " I integrated MaterialUI to craft a seamless user experience, constructing the Homepage and an Authentication Page. For the Event Management Portal, I created an admin dashboard and a dynamic form akin to Google Forms. This streamlined workflow led to a notable reduction in processing time by 40%, empowering efficient event organization. The Inventory Management Portal underwent significant enhancements with added search functionality . This overhaul resulted in a 50% boost in productivity and an impressive 70% reduction in errors. Additionally, the introduction of an application form for the Product Development Internship (PDI) streamlined the application process, reducing processing time by 60%. Finally, the implementation of an Email Notification System using Nodemailer has greatly improved applicant engagement and satisfaction, leading to a 30% increase in overall applicant interactions. These developments collectively transformed NVCTI's digital landscape, optimizing operations and significantly enhancing user experiences.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH5UGylG7_C4H321TLBJmqX8y2WPIbICoHtFEzdnYQ4g&s",
+    role: "Full Stack Web Developer Intern",
+    company: "INFOTRIXS",
+    date: "Oct 2023- Dec 2023",
+    desc: "Worked on live projects with Senior Developer fixing bugs and issues on codebase & production and Made Project 40% more efficient & Scalable. Worked on User Verification/Authentication features challenges of detecting and removing the fake bots and posts. ",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.ibb.co/8cPfjcw/download-iit-logo.jpg",
+    school: "Indian Institute of Technology (IIT) Dhanbad",
+    date: "NOV 2021 - May 2025",
+    grade: "7.83 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Mechanical Engineering at Indian Institute of  Technology, Dhanbad. I have completed 5 semesters and have a CGPA of 7.83. I have taken courses in Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, among others. I am also a member of the CodeISM and CodeElectronics at IIT Dhanbad , where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Mechanical Engineering",
+  },
+  {
+    id: 1,
+    img: "https://schools.org.in/assets/images/favicon.png",
+    school: "High School Karai Parsurai, Nalanda",
+    date: "Apr 2018 - Mar 2020",
+    grade: "90.2%",
+    desc: "I have completed my class 12th at High School Karai Parsurai, Nalanda where i have studied about Physics, Chemistry and Mathematics along with language Paper.",
+    degree: "ISC(XII), Science ",
+  },
+  {
+    id: 2,
+    img: "https://schools.org.in/assets/images/favicon.png",
+    school: "High School Karai Parsurai, Nalanda",
+    date: "Apr 2017 - Mar 2018",
+    grade: "84.2%",
+    desc: "I have completed my class 10th at High School Karai Parsurai, Nalanda where i have studied about basic Science, Mathematics, Social Studies as well as language Paper.",
+    degree: "BSEB(X), ",
+  },
+];
+
+export const projects = [
+  {
+    id: 20,
+    title: "Real Estate App ",
+    date: "Jul 2023 - Aug 2023",
+    description:
+      " Developing a state-of-the-art real estate application entails integrating advanced authentication methods like JWT, Firebase, and Google OAuth for robust user security. Leveraging MongoDB, the app facilitates seamless CRUD operations for property listings. User experience is paramount, thus incorporating features like image uploads and intuitive listing management. Cutting-edge search functionality ensures users easily locate desired properties. Lastly, deploying the MERN stack app is simplified through the 'render' platform, offering a hassle-free deployment experience.",
+    image: "https://i.postimg.cc/3R3vWxGN/Real-Estate.png",
+    tags: [
+      "ReactJS",
+      "Node Js",
+      "Express Js",
+      "MongoDb",
+      "Redux",
+      "Tailwind CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Real-Estate",
+    webapp: "https://estate-wave.onrender.com/",
+  },
+  {
+    id: 9,
+    title: "ChatApp",
+    date: "Jun 2023 - Aug 2023",
+    description:
+      " The project is a real-time platform enabling group and personal chatting with integrated video call capabilities powered by WebRTC. It incorporates robust user authentication and authorization using the MERN stack. Additionally, an AI chatbot is seamlessly integrated to enhance user experience and interaction within the platform. ",
+    image: "https://i.ibb.co/sJGsLgj/Screenshot-2023-12-11-204703.png",
+    tags: [
+      "ReactJS",
+      "AWS",
+      "WebRTC",
+      "AI/ML",
+      "Socket.io",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Video-Call-Chat-App-",
+    webapp: "https://chat-app-lyart-sigma.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Admin Dashboard",
+    date: "Apr 2023 - May 2023",
+    description:
+      "The project is a feature-rich platform, offering an intuitive user interface and robust functionalities such as user management and data visualization. Real-time data updates, including geography location charts, enhance the dynamic user experience. It efficiently integrates Redux for streamlined state management, ensuring optimal performance and responsiveness. With its focus on intuitive design and powerful features, the platform delivers a seamless experience for users, facilitating easy navigation, comprehensive data representation, and real-time insights in a responsive and dynamic environment ",
+    image: "https://i.ibb.co/r6KQmsG/Screenshot-2023-12-12-094627.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "Nivo Charts",
+      "MaterialUI",
+      "ThreeJs",
+    ],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/EDashboard",
+    webapp: "https://dashboard-x1h2.onrender.com/",
+  },
+  {
+    id: 1,
+    title: "Ecommerce App",
+    date: "Oct 2022 - Aug 2023",
+    description:
+      "The ecommerce website stands as a robust, scalable platform leveraging Strapi for efficient content and product data management. It hosts comprehensive features like product listing, cart functionality, and adept management of orders, transactions, and user authentication. Seamlessly integrated with Stripe payment gateways, it ensures secure and smooth transactions. This platform not only provides a streamlined shopping experience but also showcases the potential of utilizing Strapi for content and product data management, offering a secure, feature-rich ecommerce environment with intuitive functionalities.",
+    image: "https://i.ibb.co/chQDt1r/Screenshot-2024-03-18-215742.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Strapi", "Redux", "Stripe"],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Ecommerce-App",
+    webapp: "https://meesho-clone-ashokprjapati.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Covid 19",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Covid 19 is a dynamic platform providing real-time statistics on the COVID-19 pandemic, complete with graphical representations for easy understanding. Users can delve into the latest data and trends effortlessly, thanks to its intuitive search functionality. Developed with React.js, Material UI, and Chart.js, this website offers a visually appealing and informative experience, empowering individuals to stay informed and make informed decisions amidst the ongoing global health crisis.",
+    image: "https://i.postimg.cc/hGRgqCzb/Covid.png",
+    tags: ["Reactjs", "Axios", "Material Ui", "ChartJs"],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Covid-19-Stats",
+    webapp: "https://covid19-ph8h.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Expance Manager",
+    date: "Dec 2022 - Apr 2023",
+    description:
+      "Expense Manager simplifies financial tracking with easy-to-use statistics and graphical representations for both expenses and income. Utilizing React.js, Node.js, Express.js, MongoDB, and Ant Design, this platform offers a seamless experience for managing personal finances. With intuitive features and a user-friendly interface, users can efficiently monitor their expenditures and earnings, gaining valuable insights to effectively manage their financial health.",
+    image: "https://i.postimg.cc/mgBfXP08/Expance.png",
+    tags: ["Reactjs", "NodeJs", "Expressjs", "MongoDB", "AntDesign"],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Expance-Tracker/tree/master",
+    webapp: "https://expance1.onrender.com/",
+  },
+  {
+    id: 10,
+    title: "Movie App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "The Movie App, crafted with ReactJS and the MERN stack, stands as a dynamic platform leveraging APIs. It offers an immersive movie-watching experience by seamlessly integrating various services. Utilizing ReactJS for a responsive front-end, it connects to a MongoDB database through Node.js and Express for robust backend support. The incorporation of APIs enriches the app with extensive movie data, enabling users to browse, discover, and access comprehensive information about movies. Through its intuitive interface and powerful tech stack, this app redefines entertainment, providing a seamless platform for movie enthusiasts to explore, discover, and enjoy their favorite films.",
+    image: "https://i.postimg.cc/jSQ479QX/Screenshot-390.png",
+    tags: ["NextJS", "TMDB API", "Axios", "Tailwind CSS"],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/Movie-App",
+    webapp: "https://imdb.100jsprojects.com/",
+  },
+  {
+    id: 9,
+    title: "To-Do App",
+    date: "Dec 2023 - Jan 2024",
+    description:
+      "Introducing the To-Do App, where users can effortlessly manage daily tasks. With features to create, update, delete, mark as finished, and bulk actions like marking all or unmarking all tasks. Developed using React.js, Node.js, Express.js, and Redux Toolkit, this app ensures a seamless and efficient task management experience. Stay organized and productive with this intuitive tool designed to streamline your daily workflow.",
+    image: "https://i.postimg.cc/nrNV9nzN/Screenshot-378.png",
+    tags: ["Reactjs", "Redux", "NodeJs", "Expressjs"],
+    category: "web app",
+    github: "https://github.com/RaviSKumar210755/To-Do-App-QuadB-Tech",
+    webapp: "https://to-do-app-quadb-tech.vercel.app/",
+  },
+
+  {
+    id: 11,
+    title: "Quiz App",
+    date: "Sept 2023 - Oct 2023",
+    description:
+      "Our quiz app leverages a robust tech stack, comprising HTML, CSS, and JavaScript for front-end development, React for dynamic UI rendering, Node.js for server-side logic, Express.js for routing, PostgreSQL for database management, and Three.js for immersive visualizations.",
+    image: "https://i.ibb.co/v3fZ8bD/Screenshot-2024-03-17-114615.png",
+    tags: ["ReactJS", "API", "Axios", "NodeJS", "ThreeJS", "3D Three-Fibre"],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Quiz_Assignment?tab=readme-ov-file",
+    webapp: "https://quiz-assignment-ghuu-nkt3f1xrg-maharaj7809.vercel.app/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey" },
+  { year: 2022, text: "Worked as a freelance developer" },
+  { year: 2021, text: "Founded JavaScript Mastery" },
+  { year: 2022, text: "Shared my projects with the world" },
+  { year: 2023, text: "Started my own platform" },
+];
